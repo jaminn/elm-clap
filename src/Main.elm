4ab4960d077e78849f7d20ee41481f2691a925bf
jaminn <rnnwkals1@naver.com>
2022-01-10 03:57:51 +0900

    clear unused functions

--- src/Main.elm
+++ src/Main.elm
@@ -46,59 +46,17 @@ main =
 -- MODEL
 
 
-type alias ToggleModel =
-    { id : Int
-    , isSelected : Bool
-    }
-
-
 type alias Model =
-    { toggleUid : Int
-    , toggleList : List ToggleModel
-    , isLeft : Bool
+    { isLeft : Bool
     , myId : Maybe String
     , peerIdInput : String
     , isConnect : Bool
     }
 
 
-addToggle :
-    Bool
-    -> { a | toggleUid : Int, toggleList : List ToggleModel }
-    -> { a | toggleUid : Int, toggleList : List ToggleModel }
-addToggle isSelected model =
-    { model
-        | toggleUid = model.toggleUid + 1
-        , toggleList = model.toggleList ++ [ ToggleModel model.toggleUid isSelected ]
-    }
-
-
-removeLastToggle model =
-    { model
-        | toggleList =
-            case List.reverse model.toggleList of
-                _ :: tl ->
-                    List.reverse tl
-
-                [] ->
-                    []
-    }
-
-
-clearToggle model =
-    { model
-        | toggleList =
-            List.filter (not << .isSelected) model.toggleList
-    }
-
-
 init : () -> ( Model, Cmd Msg )
 init _ =
-    let
-        toggleList =
-            List.repeat 15 False |> List.indexedMap ToggleModel
-    in
-    ( Model (List.length toggleList) toggleList True Nothing "" False
+    ( Model True Nothing "" False
     , Cmd.none
     )
 
@@ -109,10 +67,6 @@ init _ =
 
 type Msg
     = NoOp
-    | ToggleClicked Int
-    | PlusClicked
-    | MinusClicked
-    | ClearClicked
     | ChangeStateClicked
     | TryPeerConnect
     | TrySendMessage
@@ -127,31 +81,6 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
-
-        ToggleClicked id ->
-            ( { model
-                | toggleList =
-                    List.map
-                        (\t ->
-                            if id == t.id then
-                                { t | isSelected = not t.isSelected }
-
-                            else
-                                t
-                        )
-                        model.toggleList
-              }
-            , Cmd.none
-            )
-
-        PlusClicked ->
-            ( addToggle False model, Cmd.none )
-
-        MinusClicked ->
-            ( removeLastToggle model, Cmd.none )
-
-        ClearClicked ->
-            ( clearToggle model, Cmd.none )
 
         ChangeStateClicked ->
             ( { model | isLeft = not model.isLeft }, sendMessage "change" )
@@ -200,117 +129,6 @@ subscriptions model =
 
 floatDiv =
     node "float-div"
-
-
-toggleListView : List ToggleModel -> Html Msg
-toggleListView toggleList =
-    Keyed.node "div"
-        [ css [ position absolute, top zero, left zero ] ]
-        (List.map
-            (\t ->
-                ( String.fromInt t.id
-                , floatDiv
-                    [ Attr.property "targetEleId" (E.string ("toggle" ++ String.fromInt t.id))
-                    , Attr.property "isTransitionActive" (E.bool True)
-                    ]
-                    [ lazy2 toggle (ToggleClicked t.id) t.isSelected ]
-                )
-            )
-            toggleList
-        )
-
-
-plusButton =
-    div
-        [ css
-            [ backgroundColor (hex "#fff")
-            , width (px 30)
-            , height (px 30)
-            , textAlign center
-            , borderRadius (px 10)
-            , display inlineBlock
-            ]
-        ]
-        [ div
-            [ css
-                [ fontSize (px 22)
-                , transforms [ translateY (px -1) ]
-                , hover [ color (hex "#333") ]
-                ]
-            ]
-            [ text "+" ]
-        ]
-
-
-minusButton =
-    div
-        [ css
-            [ backgroundColor (hex "#fff")
-            , width (px 30)
-            , height (px 30)
-            , textAlign center
-            , borderRadius (px 10)
-            , display inlineBlock
-            ]
-        ]
-        [ div
-            [ css [ fontSize (px 22), transforms [ translateY (px -1) ] ] ]
-            [ text "-" ]
-        ]
-
-
-clearButton =
-    div
-        [ css
-            [ backgroundColor (hex "#fff")
-            , height (px 30)
-            , textAlign center
-            , borderRadius (px 10)
-            , display inlineBlock
-            ]
-        ]
-        [ div
-            [ css [ fontSize (px 18), padding (px 5), lineHeight (num 1) ] ]
-            [ text "clear" ]
-        ]
-
-
-view2 : Model -> Html Msg
-view2 model =
-    div [ css [ position absolute ] ]
-        [ floatDiv
-            [ Attr.property "targetEleId" (E.string "toggleBox")
-            , Attr.property "isTransitionActive" (E.bool True)
-            , css
-                [ borderRadius (px 10)
-                , backgroundColor (hex "#ddd")
-                , boxShadow4 (px 1) (px 1) (px 5) (hex "#999")
-                , zIndex (int -1)
-                ]
-            ]
-            []
-        , div
-            [ id "toggleBox"
-            , css
-                [ padding (px 10)
-                , margin (px 20)
-                ]
-            ]
-            [ div
-                [ css [ paddingBottom (px 5) ] ]
-                [ text ("토글 갯수 : " ++ String.fromInt (List.length model.toggleList)) ]
-            , div
-                [ css [ paddingBottom (px 5) ] ]
-                [ text ("선택된 토글 갯수 : " ++ String.fromInt (List.length (List.filter .isSelected model.toggleList))) ]
-            , div [ css [] ] (List.map (\t -> div [ id ("toggle" ++ String.fromInt t.id), css [ height (px 20), width (px 60), marginTop (px 10) ] ] []) model.toggleList)
-            , toggleListView model.toggleList
-            , div [ css [ marginTop (px 5) ] ]
-                [ div [ onClick PlusClicked, css [ display inlineBlock, padding (px 3) ] ] [ plusButton ]
-                , div [ onClick MinusClicked, css [ display inlineBlock, padding (px 3) ] ] [ minusButton ]
-                ]
-            , div [ onClick ClearClicked, css [ display inlineBlock, padding (px 3) ] ] [ clearButton ]
-            ]
-        ]
 
 
 view : Model -> Html Msg
